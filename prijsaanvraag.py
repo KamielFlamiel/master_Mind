f12dbbdc71f1754037f685c01332ef3ae413b303
KamielFlamiel <justinmeijer2007@outlook.com>
2025-05-21 08:35:58 +0200

    devops opdrachten gemaakt

---
+++ prijsaanvraag.py
@@ -0,0 +1,7 @@
+#!/bin/python3
+prijs_aanvraag = float(input("Wat is de prijs?"))
+procent = 21
+print(prijs_aanvraag * (procent / 100))
+
+
+
---
+++ wiebenjij.py
@@ -0,0 +1,13 @@
+#!/bin/python3
+telefoon_merk = input("Welk telefoon merk heb jij? ")
+laptop_merk = input("Welk laptop merk heb jij? ")
+leeftijd = int(input("Hoe oud ben jij? "))
+favo_vak = input("Wat is je favoriete vak? ")
+print(telefoon_merk)
+print(laptop_merk)
+print(leeftijd)
+print(favo_vak)
+
+
+driejaar = int(leeftijd + 3)
+print("over 3 jaar ben je " ,driejaar)
