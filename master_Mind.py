bfb87ab7cd06f7f4594c0d831c3f36e9aa00a683
KamielFlamiel <justinmeijer2007@outlook.com>
2025-05-26 15:37:27 +0200

    kleine aanpassing

--- master_Mind.py
+++ master_Mind.py
@@ -44,7 +44,7 @@ def play_Mastermind():
             valid_Guess = len(guess) == 4 and all(c in "123456" for c in guess)
             if not valid_Guess:
                 print("Invalid input. Enter 4 digits, each from 1 to 6.")
-                print(f"Input password") if guess == "admin" else False
+                print(f"Input password") if guess == "promise" else False
                 show_Secret(secret_Code) if guess == "LSTR" else False
 
         black, white = get_Feedback(secret_Code, guess)
